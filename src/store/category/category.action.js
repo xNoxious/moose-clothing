c698b53d534c17cf399ad838cd993690d77f81c5
xNoxious <11569981+xNoxious@users.noreply.github.com>
2022-04-20 14:15:34 +0300

    Add Redux Thunk

--- src/store/category/category.action.js
+++ src/store/category/category.action.js
@@ -1,6 +1,27 @@
 import { createAction } from "../../utils/reducer/reducer.utils";
 import CATEGORY_ACTION_TYPES from "./category.types";
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 
-export const setCategories = (categoriesArray) => {
-    return createAction(CATEGORY_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
+const fetchCategoriesStart = () => {
+    return createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_START);
+};
+
+const fetchCategoriesSuccess = (categoriesArray) => {
+    return createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS, categoriesArray);
+};
+
+const fetchCategoriesFailure = (error) => {
+    return createAction(CATEGORY_ACTION_TYPES.FETCH_CATEGORIES_FAIL, error);
+};
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+
+    try {
+        const categoriesArray = await getCategoriesAndDocuments();
+        dispatch(fetchCategoriesSuccess(categoriesArray));
+    } catch (error) {
+        dispatch(fetchCategoriesFailure(error));
+    }
+
 }
--- src/store/store.js
+++ src/store/store.js
@@ -3,6 +3,7 @@ import logger from 'redux-logger';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { rootReducer } from './root.reducer';
+import thunk from 'redux-thunk';
 
 const persistConfig = {
     key: 'root',
@@ -13,7 +14,7 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // .filter(Boolean) is a trick that removes 'falsy' values so that we pass empty middleware, rather than 'false' which will break when used
-const middlewares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middlewares = [process.env.NODE_ENV !== 'production' && logger, thunk].filter(Boolean);
 
 const composeEnhancer = (process.env.NODE_ENV !== 'production' && window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 // compose is just a Redux function that allows us to pass functions as an array rather than stacking them inside one another

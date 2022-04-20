00f87d3cc8c91907b6cf68b866a108bbbf4cbd11
xNoxious <11569981+xNoxious@users.noreply.github.com>
2022-04-20 22:30:27 +0300

    Update authentication.component.jsx

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -5,8 +5,8 @@ import './authentication.styles.scss';
 const Authentication = () => {
     return (
         <div className="authentication-container">
-            <SignUpForm />
             <SignInForm />
+            <SignUpForm />
         </div>
     )
 }
